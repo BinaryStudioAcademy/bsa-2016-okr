7510f1fc2d9d2d17d31e6bb2d6238d8ff00e3ad6
Evgeniy331 <jknoxville712@gmail.com>
2016-08-03 21:30:50 +0300

    repositories: little fix for the name of schemes in repositories

--- repositories/user.js
+++ repositories/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Repository = require('../units/Repository');
-var User = require('../schemas/tUser').model;
+var User = require('../schemas/user').model;
 
 var UserRepository = function(){
 	Repository.prototype.constructor.call(this);
