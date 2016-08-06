3fa3e668299f3449067ed0c11ea124854de74e3b
94117nl <rttededersixtwo@gmail.com>
2016-08-07 01:33:01 +0300

    backend: updated category route & service

--- backend/routes/api/category.js
+++ backend/routes/api/category.js
@@ -1,32 +1,66 @@
 const router = require('express').Router();
 const repository = require('../../repositories/category');
 const dbCallback = require('./response');
+const session = require('../../config/session');
 const categoryService = require('../../services/category');
 
 router.get('/', (req, res, next) => {
 	repository.getAll(dbCallback(res));
 });
 
+router.get('/deleted', (req, res, next) => {
+	repository.getAllDeleted(dbCallback(res));
+});
+
 router.post('/', (req, res, next) => {
-	// TODO admin check
 	console.log("Category rest POST hit");
+/*
+	if(!session.isAdmin) {
+		var err = new Error('You are not allowed to do this');
+		err.status = 403;
+		return dbCallback(err);
+	}
+	*/
 	console.log(req.body);
 	var title = req.body.title;
 	if(title != undefined && title.length > 0) {
-		var obj = {title: title, isDeleted: 0};
-		categoryService.add(obj, dbCallback(res));
+		console.log(title);
+
+		var data = {
+			title: title,
+			isDeleted: false
+		}
+
+		categoryService.add(data, dbCallback(res));
+		// the response is empty now
 	}else {
-	res = 'Enter title';
+		/// this doesn't work
+		var err = new Error('Title must be set');
+		return dbCallback(err);
 	}
 });
 
 router.put('/:id', (req, res, next) => {
-	// TODO admin check
+	/*
+		if(!session.isAdmin) {
+			var err = new Error('You are not allowed to do this');
+			err.status = 403;
+			return dbCallback(err);
+		}
+		*/
+		//TODO add title verefication
 	repository.update(req.params.id, req.body, dbCallback(res));
 });
 
 router.delete('/:id', (req, res, next) => {
-	// TODO admin check
+	/*
+		if(!session.isAdmin) {
+			var err = new Error('You are not allowed to do this');
+			err.status = 403;
+			return dbCallback(err);
+		}
+		*/
+		//for now soft delete only
 	repository.update(req.params.id, {isDeleted: 1}, dbCallback(res));
 	//repository.delete(req.params.id, dbCallback(res));
 });
--- backend/services/category.js
+++ backend/services/category.js
@@ -1,9 +1,11 @@
-var CategoryRepository = require('../repositories/category.js');
+var CategoryRepository = require('../repositories/category');
+var HistoryRepository = require('../repositories/history');
+var async = require('async');
 
-var CategoryService = function() {
-};
+var CategoryService = function() {};
 
 CategoryService.prototype.add = function(title, callback){
+  /*
   console.log("CategoryService hit");
   CategoryRepository.add(title, function(err, data){
     if (err){
@@ -12,4 +14,31 @@ CategoryService.prototype.add = function(title, callback){
     console.log("CategoryRepository hit");
     callback(err, null);
   });
+ console.log("check history");
+ console.log(title);
+  HistoryRepository.add(title, function(err, data){
+    if (err){
+      return callback(err, null);
+    };
+    console.log("CategoryRepository hit");
+    callback(err, null);
+  });
+*/
+/*
+ 	async.waterfall([
+ 		addCategory,
+ 		addEventToHistory,
+ 	], (err, result) => {
+ 		callback(err);
+ 	});
+*/
+
+  CategoryRepository.add(title, callback);
+    var history = {
+      type: "added new category",
+    }
+  //HistoryRepository.add(history, callback);
+
 };
+
+module.exports = new CategoryService();
