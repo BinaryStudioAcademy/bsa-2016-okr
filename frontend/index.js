4309576e4936bff9a8f90dfb8f2c211575390522
Nikita Ivanchenko <mivansevdon@gmail.com>
2016-08-07 12:17:20 +0300

    1) Fixed merge conflicts 2) Changed frontend structure

--- frontend/index.js
+++ frontend/index.js
@@ -3,13 +3,14 @@ import { render } from "react-dom"
 import App from "./containers/app"
 import LoginPage from "./components/login-page.js"
 import {IndexRoute, Route, Router, browserHistory} from 'react-router';
+import RecycleBin from './components/RecycleBin/RecycleBin.js';
 
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
-import reducer from './components/reducers/commonReducer'
+import reducer from './reducers/commonReducer'
 
 const middleware = [ thunk ]
 
@@ -23,6 +24,7 @@ render(
       <Router history={browserHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={LoginPage} />
+		  <Route path="recycle-bin" component={RecycleBin}/>
         </Route>
       </Router>
     </Provider>)
