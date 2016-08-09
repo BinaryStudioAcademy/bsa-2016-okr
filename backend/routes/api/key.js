8743dfcffda87589e69b1461bc93a493df45eb34
Yura Golomb <yuragolomb@gmail.com>
2016-08-09 11:12:07 -0700

    team/backend: added id validation

--- backend/routes/api/key.js
+++ backend/routes/api/key.js
@@ -1,17 +1,30 @@
 const router = require('express').Router();
 const repository = require('../../repositories/key');
-const service = require('../../services/key.js')
+const service = require('../../services/key')
+const ValidateService = require('../../utils/ValidateService');
 
 router.post('/', (req, res, next) => {
 	repository.add(req.body, res.callback);
 });
 
 router.get('/objective/:id', (req, res, next) => {
-	repository.getByObjId(req.params.id, res.callback);
+	var id = req.params.id;
+
+	if(id !== session._id && !userMentorRepository.checkUserMentor(id, session._id)) {
+		return res.forbidden();
+	};
+
+	repository.getByObjId(id, res.callback);
 });
 
 router.put('/:id', (req, res, next) => {
-	repository.update(req.params.id, req.body, res.callback);
+	var id = req.params.id;
+
+	if(id !== session._id && !userMentorRepository.checkUserMentor(id, session._id)) {
+		return res.forbidden();
+	};
+	
+	repository.update(id, req.body, res.callback);
 });
 
 router.get('/title/:title', (req, res, next) => {
@@ -19,11 +32,23 @@ router.get('/title/:title', (req, res, next) => {
 });
 
 router.put('/:id/approve', (req, res, next) => {
-	service.changeApprove(req.params.id, res.callback);
+	var id = req.params.id;
+
+	if(id !== session._id && !userMentorRepository.checkUserMentor(id, session._id)) {
+		return res.forbidden();
+	};
+	
+	service.changeApprove(id, res.callback);
 });
 
 router.delete('/:id', (req, res, next) => {
-	repository.delete(req.params.id, res.callback);
+	var id = req.params.id;
+
+	if(id !== session._id && !userMentorRepository.checkUserMentor(id, session._id)) {
+		return res.forbidden();
+	};
+	
+	repository.delete(id, res.callback);
 });
 
 module.exports = router;
