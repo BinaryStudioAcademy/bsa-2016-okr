c6ae424d999bfd93f01651a5fb52f81b997e0793
Yura Golomb <yuragolomb@gmail.com>
2016-08-09 21:55:12 -0700

    team/backend: implementing history events on keyResults

--- backend/routes/api/key.js
+++ backend/routes/api/key.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const repository = require('../../repositories/key');
 const service = require('../../services/key');
 const ValidateService = require('../../utils/ValidateService');
+const adminOnly = require('../adminOnly');
+const session = require('../../config/session')
 
 router.post('/', (req, res, next) => {
-	repository.add(req.body, res.callback);
+	service.add(session._id, req.body, res.callback);
 });
 
 router.put('/:id', (req, res, next) => {
@@ -14,7 +16,7 @@ router.put('/:id', (req, res, next) => {
 		return res.badRequest();
 	};
 	
-	repository.update(id, req.body, res.callback);
+	service.update(session._id, id, req.body, res.callback);
 });
 
 router.delete('/:id', (req, res, next) => {
@@ -24,7 +26,7 @@ router.delete('/:id', (req, res, next) => {
 		return res.badRequest();
 	};
 	
-	repository.delete(id, res.callback);
+	service.delete(session._id, id, res.callback);
 });
 
 router.get('/objective/:id', (req, res, next) => {
@@ -42,14 +44,14 @@ router.get('/title/:title', (req, res, next) => {
 	service.autocomplete(req.params.title, res.callback);
 });
 
-router.put('/:id/approve', (req, res, next) => {
+router.put('/:id/approve', adminOnly, (req, res, next) => {
 	var id = req.params.id;
 
 	if(!ValidateService.isCorrectId(id)) {
 		return res.badRequest();
 	};
 	
-	service.changeApprove(id, res.callback);
+	service.changeApprove(session._id, id, res.callback);
 });
 
 
