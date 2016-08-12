827c163401a84a0bbb2d582f783c441221a0a8be
Boris <transkarpation@gmail.com>
2016-08-12 09:26:34 +0300

    recycleBin: capital leter

--- frontend/components/admin/RecycleBin/deletedPlansItem.js
+++ frontend/components/admin/RecycleBin/deletedPlansItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+ 
 class DeletedPlansItem extends Component {
 
 	constructor(props) {
--- frontend/components/admin/RecycleBin/deletedTmplsItem.js
+++ frontend/components/admin/RecycleBin/deletedTmplsItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+ 
 class DeletedTmplsItem extends Component {
 
 	constructor(props) {
