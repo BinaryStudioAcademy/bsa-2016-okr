0d7df4337e68a304543635563a25a7d3627aab45
petrushkas <C:\Users\\AppData\Roaming\The Bat!>
2016-08-13 19:24:45 +0300

    some style changes

--- frontend/components/home-page.js
+++ frontend/components/home-page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './home-page/home-page.scss';
-import ListOfUsers from './list-of-users/list-of-users.js';
+import ListOfUsers from './list-of-users/list-of-users.jsx';
 import Quarter from './home-page/quarter.jsx';
 import UserObjectives from './home-page/objectives.jsx';
 import CentralWindow from '../containers/central-window.jsx';
