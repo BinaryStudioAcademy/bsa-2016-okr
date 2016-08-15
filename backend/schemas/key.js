51715fe4edbdc0478ec452661734e262d384e63a
Yura Golomb <yuragolomb@gmail.com>
2016-08-15 20:24:59 +0300

    team/backend: 1) changed db schemas for KeyResult and User 2) added schema for Quarter

--- backend/schemas/key.js
+++ backend/schemas/key.js
@@ -2,12 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var keyResultSchema = new Schema({
-	objectiveId: {type: Schema.Types.ObjectId, ref: 'Objective'},
-	title: String,
-	forks: Number,
-	difficulty: String,
-	isApproved: Boolean,
-	isDeleted: Boolean
+	title: String
+	// objectiveId: {type: Schema.Types.ObjectId, ref: 'Objective'},
+	// title: String,
+	// forks: Number,
+	// difficulty: String,
+	// isApproved: Boolean,
+	// isDeleted: Boolean
 }, {
     timestamps: true
 });
--- backend/schemas/user.js
+++ backend/schemas/user.js
@@ -2,26 +2,28 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-	followObjectives: [
-		{userId: {type: Schema.Types.ObjectId, ref: 'User'},
-		objectiveId: {type: Schema.Types.ObjectId, ref: 'Objective'}
-	}],
-	objectives: [{
-		quarter: String,
-		year: String,
-		description: String,
-		objectiveId: {type: Schema.Types.ObjectId, ref: 'Objective'},
-		keys: [{keyId: {type: Schema.Types.ObjectId, ref: 'Key'},
-				score: Number,
-		}],
-		category: {type: Schema.Types.ObjectId, ref: 'Category'},
-		isDeleted: Boolean,
-		isArchived: Boolean,
-		feedback: String
-	}],
-	lastVisitDate: Date,
-	historyWatchDate: Date,
-	isDeleted: Boolean
+	localRole: String,
+	mentor: {type: Schema.Types.ObjectId, ref: 'User'
+	// followObjectives: [
+	// 	{userId: {type: Schema.Types.ObjectId, ref: 'User'},
+	// 	objectiveId: {type: Schema.Types.ObjectId, ref: 'Objective'}
+	// }],
+	// objectives: [{
+	// 	quarter: String,
+	// 	year: String,
+	// 	description: String,
+	// 	objectiveId: {type: Schema.Types.ObjectId, ref: 'Objective'},
+	// 	keys: [{keyId: {type: Schema.Types.ObjectId, ref: 'Key'},
+	// 			score: Number,
+	// 	}],
+	// 	category: {type: Schema.Types.ObjectId, ref: 'Category'},
+	// 	isDeleted: Boolean,
+	// 	isArchived: Boolean,
+	// 	feedback: String
+	// }],
+	// lastVisitDate: Date,
+	// historyWatchDate: Date,
+	// isDeleted: Boolean
 }, {
 	timestamps: true
 });
