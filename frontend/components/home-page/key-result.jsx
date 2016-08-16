b7ea5c98e2b5363301325e28550429ec2f9f4d90
Ihor Oziian <ihor.oziian@gmail.com>
2016-08-16 13:37:22 +0300

    Change style key-result score
    1) add Material white color for global use;
    2) change style key-result score on homepage and user objectives;
    3) delete component key-result(s), it doesn't need now! Every folder have itself;

--- frontend/components/home-page/key-result.jsx
+++ frontend/components/home-page/key-result.jsx
@@ -1,52 +1,52 @@
 import React, { Component } from 'react';
 
 class KeyResult extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         keyResult: props.item
-      }
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyResult: props.item
+        }
 
-      this.changeScore = this.changeScore.bind(this);
-   }
+        this.changeScore = this.changeScore.bind(this);
+    }
 
-   changeScore(e) {
-   var score = e.target.value;
-   this.state.keyResult.score = score;
-      this.setState({
-        keyResult: this.state.keyResult
-      })
+    changeScore(e) {
+        var score = e.target.value;
+        this.state.keyResult.score = score;
+        this.setState({
+            keyResult: this.state.keyResult
+        })
 
-      if (e.target.value == 1)
-         e.target.previousElementSibling.classList.add('completed');
-      else
-         e.target.previousElementSibling.classList.remove('completed')
-      this.props.changeScore(this.state.keyResult);
+        if (e.target.value == 1)
+            e.target.previousElementSibling.classList.add('completed');
+        else
+            e.target.previousElementSibling.classList.remove('completed')
+        this.props.changeScore(this.state.keyResult);
 
-   }
+    }
 
-   render() {
-   if (this.state.keyResult.score == 1)
-      return (
-         <li className="key-result">
-            <div><span className='completed'>{this.props.item.title}</span></div>
-            
-            <input type="range" min="0" max="1" step="0.1" className="keyScore"
-                        value={this.state.keyResult.score}  onChange={this.changeScore}/>
-              <div><span className='score'>{this.state.keyResult.score}</span></div>          
-          </li>        
-      )
-      else
-         return (
-            <li className="key-result">
-              <div><span>{this.props.item.title}</span></div>
-            
-               <input type="range" min="0" max="1" step="0.1" className="keyScore"
-                        value={this.state.keyResult.score}  onChange={this.changeScore}/>
-                   <div><span className='score'>{this.state.keyResult.score}</span></div>      
-             </li>        
-         )
-   }
+    render() {
+        if (this.state.keyResult.score == 1)
+            return (
+                <li className="key-result">
+                    <div><span className='completed'>{this.props.item.title}</span></div>
+
+                    <input type="range" min="0" max="1" step="0.1" className="keyScore"
+                           value={this.state.keyResult.score} onChange={this.changeScore}/>
+                    <div><span className='score'>{this.state.keyResult.score}</span></div>
+                </li>
+            )
+        else
+            return (
+                <li className="key-result">
+                    <div><span>{this.props.item.title}</span></div>
+
+                    <input type="range" min="0" max="1" step="0.1" className="keyScore"
+                           value={this.state.keyResult.score} onChange={this.changeScore}/>
+                    <div><span className='score'>{this.state.keyResult.score}</span></div>
+                </li>
+            )
+    }
 }
 
 export default KeyResult
