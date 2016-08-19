cce178b2fcacb9d714aa2f98355f46b277baa2e4
TrishGitHub <i.noschenko@gmail.com>
2016-08-20 00:12:30 +0300

    editing

--- frontend/components/admin/OKRmanaging/components/ObjectiveList.js
+++ frontend/components/admin/OKRmanaging/components/ObjectiveList.js
@@ -5,7 +5,7 @@ export default ({ data, update, remove, editing, edit, editingDone, editingChang
   if (!data) { return (<p>Loading...</p>); }
 
   const objectives = data.map((objective, index) => {
-    return (<ObjectiveData objective={objective} key={index} index={index+1} update={update} remove={remove} editing={editing} edit={edit} editingDone={editingDone} editingChange={editingChange}/>);
+    return (<ObjectiveData objective={objective} key={index} index={index} update={update} remove={remove.bind(null, objective)} editing={editing} edit={edit} editingDone={editingDone} editingChange={editingChange}/>);
   });
 
   return (
