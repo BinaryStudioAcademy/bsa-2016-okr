9ccdf4b4de881be64135e15b74d74cf404d1ee37
Roman <romansahan4@gmail.com>
2016-08-27 00:32:41 +0300

    other person page: added button 'Take apprentice' for admin and mentor only

--- frontend/components/other-persons-page/persons-info.js
+++ frontend/components/other-persons-page/persons-info.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react'
 import Quarter from './persons-quarter.js'
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as actions from "../../actions/otherPersonActions.js";
 
 class PersonsInfo extends Component {
+	constructor(props){
+		super(props);
+
+	}
+
 	render() {
+		let apprentice;
+		console.log(this.props.localRole)
+		if (this.props.localRole == 'mentor' || this.props.localRole == 'admin') {
+			apprentice = (<button className="btn btn-blue-hover apprentice" title="apprentice">Take apprentice</button>);
+		}
+		const {user} = this.props.user
 		var mentor = '';
-		if(this.props.data.mentor == null)
+		if(user.mentor == null)
 			mentor = 'Is not assigned';
-		else mentor = (this.props.data.mentor.userInfo.firstName +' '+ this.props.data.mentor.userInfo.lastName)
+		else mentor = (user.mentor.userInfo.firstName +' '+ user.mentor.userInfo.lastName);
 		return (
 			<div id='topPanel'>
 				<div className='userInfo'>
-				<button className="btn btn-blue-hover apprentice" title="apprentice">Take apprentice</button>
+					{apprentice}
 					<div className='logo'>
 						<img src="https://pp.vk.me/c626130/v626130341/22c8c/jg0oHo3TYWs.jpg"/>
 					</div>
 					<div className='credentials'>
-					{this.props.data.userInfo.firstName} {this.props.data.userInfo.lastName}
+					{user.userInfo.firstName} {user.userInfo.lastName}
 						<div>
 							<img src='https://pp.vk.me/c628730/v628730341/2e5d5/GGZg2j32zm4.jpg'/>
 							<span className='mentorName'>
@@ -30,4 +45,18 @@ class PersonsInfo extends Component {
 	}
 }
 
-export default PersonsInfo
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators(actions, dispatch);
+}
+
+function mapStateToProps(state, ownProps) {
+	let localRole = state.myState.me.localRole || '';
+	return {
+		localRole,
+		user: state.userPage,
+		categories: state.categories
+	};
+}
+
+const PersonsInfoConnected = connect(mapStateToProps, mapDispatchToProps)(PersonsInfo);
+export default PersonsInfoConnected
