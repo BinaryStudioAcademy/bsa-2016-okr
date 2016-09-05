524c39e465b12da783604919594ea8790b3d688f
Vitalii Petrushka <vpetrushka52@gmail.com>
2016-09-05 17:57:49 +0300

    objectives-count styles modified

--- frontend/components/common/objective/objective-list.jsx
+++ frontend/components/common/objective/objective-list.jsx
@@ -66,8 +66,8 @@ class ObjectiveList extends React.Component{
 					<div key={ index } id={ category.title }>
 					<p className="category-title-container" onClick={ ()=>this.changeCategoryContentVisibility(category.title) }>
 						<span className="category-title">
-							<i className="fi flaticon-folder-10" id={category.title+' icon'} aria-hidden="true"></i>
-							{ category.title } ({ objectiveItems.length })
+							<i className="fi flaticon-folder-10" id={category.title +' icon'} aria-hidden="true"></i>
+							{ category.title } <span className="obj-count-panel">{ objectiveItems.length }</span>
 						</span>
 					</p>
 						<div className="visible-category-content" >
