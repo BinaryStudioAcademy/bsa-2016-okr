6e4a426d5802da15aec8302cbbc8417a60e69150
Roman <romansahan4@gmail.com>
2016-09-11 17:09:31 +0300

    Global styles: changed distance between table and filters Global: Fixed seed function

--- backend/seed/seedCollections.js
+++ backend/seed/seedCollections.js
@@ -378,7 +378,7 @@ function setDefaultKeyResultsForObjectives(objectives, keyResults) {
 function getQuarters(users, userObjectives) {
 	var currentYear = CONST.currentYear;
 	var quarters = [1, 2, 3, 4];
-	var years = [currentYear, currentYear + 1];
+	var years = [currentYear];
 	var res = [];
 
 	users.forEach((user) => {
